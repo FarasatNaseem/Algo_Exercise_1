d8b61eac60c84739aab92d2d12048e52f70b9f83
Farasat Naseem <57754078+FarasatNaseem@users.noreply.github.com>
2021-03-03 13:23:35 +0100

    Add files via upload

---
+++ Exercise_1_Algos/HashTable.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "Aktie.h"
+class HashTable
+{
+	const static int TABLELENGTH = 1000;
+
+
+private: Aktie Aktien[TABLELENGTH];
+
+
+private: int CalculateHashValue()
+{
+	// Logic.
+	return 0;
+}
+	  
+
+public: void Add(Aktie& aktie)
+{
+	// Logic
+}
+
+	  void Delete(string kürzel)
+	  {
+		  // Logic
+	  }
+
+	  void Search(string kürzel)
+	  {
+		  // Logic
+	  }
+};
+
---
+++ Exercise_1_Algos/Price.h
@@ -0,0 +1,6 @@
+#pragma once
+class Price
+{
+	// Linked list.
+};
+
