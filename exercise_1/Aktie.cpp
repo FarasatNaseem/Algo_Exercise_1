cd8d9159d8af176ae3b246b0c4b3dffb3d387eb3
Farasat Naseem <57754078+FarasatNaseem@users.noreply.github.com>
2021-03-03 13:20:47 +0100

    Add files via upload

---
+++ exercise_1/Aktie.cpp
@@ -0,0 +1,1 @@
+#include "Aktie.h"
---
+++ exercise_1/Exercise_1_Algos.cpp
@@ -0,0 +1,13 @@
+// Exercise_1_Algos.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
