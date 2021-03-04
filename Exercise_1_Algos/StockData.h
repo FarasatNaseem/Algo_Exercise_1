aff25a62a8672c0f311fe2aab6ecfa0240624b1d
uzifuz <uzifuzi@gmx.at>
2021-03-04 15:27:14 +0100

    StockData
    implemented StockData Constructor
    and class members

--- Exercise_1_Algos/StockData.h
+++ Exercise_1_Algos/StockData.h
@@ -2,20 +2,36 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 #include <assert.h>
+#include <stdlib.h>
 class StockData //linked list of stock data imported from .csv file
 {
-    // Date,Open,High,Low,Close,Adj Close,Volume
+
 public:
-    std::string Data;
+    //std::string Data; //placeholder
+    std::string Date;
+    double Price[5]; //Open,High,Low,Close,Adj Close
+    int Volume;
     StockData *next;
 #
 
 public:
     StockData(std::string data)
     {
-        this->Data = data;
+        //this->Data = data;
         this->next = nullptr;
+
+        std::istringstream ds(data);
+        std::getline(ds, this->Date, ',');
+        std::string tmp;
+        for (size_t i = 0; i < 5; i++)
+        {
+            std::getline(ds, tmp, ',');
+            this->Price[i] = std::stof(tmp);
+        }
+        std::getline(ds, tmp, ',');
+        this->Volume = std::stoi(tmp);
     }
     //imports stock data from a .csv file and stores it in a linked list
     static StockData *getStockDataFromFile(std::string path)
@@ -26,7 +42,7 @@ public:
         inFile.open(path);
         assert(inFile.is_open());
         inFile >> line; //skip to first dataset
-        inFile >> line;
+        inFile >> line; //space between Adj Close
         inFile >> line;
         StockData *head = new StockData(line),
                   *p1 = head;
